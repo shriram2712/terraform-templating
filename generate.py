08574781b9e60983e10e447d221734d9eda3b377
Shriram Rajaraman <shriram.rajaraman@hashicorp.com>
2024-03-01 02:30:06 +0530

    Updated templates

--- generate.py
+++ generate.py
@@ -4,6 +4,8 @@ import shutil
 import json
 from distutils.dir_util import copy_tree
 import yaml
+import tarfile
+import requests
 
 environment = Environment(loader=FileSystemLoader("templates/"))
 accounts = []
@@ -49,3 +51,41 @@ for account in accounts:
     shutil.copyfile("./templates/outputs-tf-jinja.txt", path+"/outputs.tf")
     
     copy_tree ("./modules", path+"/modules")
+    print(path)
+    shutil.make_archive(path,format="gztar", root_dir=path)
+
+    API_ENDPOINT = "https://app.terraform.io/api/v2/organizations/{ORG_NAME}/workspaces"
+    CONFIG_VERSION_ENDPOINT = "https://app.terraform.io/api/v2/workspaces"
+    api_token = os.getenv('TF_API_TOKEN')
+    
+    headers = {
+    "Authorization": f"Bearer {api_token}",
+    "Content-Type": "application/vnd.api+json",
+    }    
+
+    print(path)
+    workspace_name = account["account_id"]
+
+    try:
+        response = requests.get(API_ENDPOINT.format(ORG_NAME="shriramrajaraman")+"/"+workspace_name, headers=headers)
+        workspace_id = response.json()["data"]["id"]
+        print(path)
+        print(workspace_id)
+        config_version = {"data":{"type":"configuration-versions"}}
+        json_config_version = json.dumps(config_version)
+        response = requests.post(CONFIG_VERSION_ENDPOINT+"/"+workspace_id+"/configuration-versions", headers=headers, data=json_config_version)
+        upload_url = response.json()
+        upload_url = response.json()["data"]["attributes"]["upload-url"]
+
+        headers_octet_stream = {
+            "Content-Type": "application/octet-stream"
+        }
+
+        with open(path+".tar.gz", 'rb') as data:
+            data_binary = data.read()
+
+        response = requests.put(upload_url, headers=headers_octet_stream, data=data_binary)
+
+        print("Run Triggered for workspace ", account["account_id"])
+    except requests.exceptions.RequestException as e:
+        print(e)
